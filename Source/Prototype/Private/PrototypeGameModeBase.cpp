7eab616787f97870f623272c4a8d7edd12e6efe0
raydEx <leijun@wangyuan.com>
2018-01-16 15:06:28 +0800

    game mode和 Character Bp设置

--- Source/Prototype/Private/PrototypeGameModeBase.cpp
+++ Source/Prototype/Private/PrototypeGameModeBase.cpp
@@ -1,14 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PrototypeGameModeBase.h"
-
+#include "PrototypeCharacter.h"
+#include "UObject/ConstructorHelpers.h"
 
 APrototypeGameModeBase::APrototypeGameModeBase()
 {
-	//// set default pawn class to our Blueprinted character
-	//static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
-	//if (PlayerPawnBPClass.Class != NULL)
-	//{
-	//	DefaultPawnClass = PlayerPawnBPClass.Class;
-	//}
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/BP/MainCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
 }
